75612fcbd04f9fd13ff1b3a57677fa22cff87e64
Theggv <m11rrrrr123@gmail.com>
2023-12-28 23:24:10 +0500

    Adjusted meta tags

--- src/routes/leaderboards/+layout.server.ts
+++ src/routes/leaderboards/+layout.server.ts
@@ -4,9 +4,9 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async () => {
   return {
     metatags: {
-      title: `Leaderboards | Players | ${SITE_NAME}`,
+      title: `Leaderboards | ${SITE_NAME}`,
       openGraph: {
-        title: `Leaderboards | Players | ${SITE_NAME}`,
+        title: `Leaderboards | ${SITE_NAME}`,
       },
     },
   };
